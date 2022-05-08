b01b400eedbd70cd2900cd000fa38da3bf570ba3
janraj00 <janrajczyk20@gmail.com>
2022-05-08 20:10:10 +0200

    pszczoly prawie działające

---
+++ bees_algorithm.py
@@ -0,0 +1,119 @@
+import random
+from typing import Dict
+import json
+import basic_solve
+
+
+def bees_algorithm(ns: int, ne: int, nb: int, nre: int, nrb: int, test_data: Dict, neighbourhood_size,
+                   epsilon: float = 12,
+                   max_iters=10):
+    """
+    :param ns: number of scouts
+    :param ne: number of elite solutions
+    :param nb: number of best solutions
+    :param nre: number of foragers for each elite solution
+    :param nrb: number of foragers for each best, but not elite solution
+    :param test_data: dictionary with test data (start position, list, shops, weights)
+    :param epsilon: epsilon to check if solution is near to previous one - stop condition
+    :param max_iters: maximal number of iterations
+    :param neighbourhood_size: Euclidean distance in which we do local search
+    """
+
+    shops = {}
+    start = test_data['start']
+    weights = test_data['weights']
+    products_list = set(test_data['list'])
+
+    for shop in test_data['shops']:
+        shops[shop['id']] = shop
+
+    def solve():
+        # initial population is top solutions from basic solve
+        initial_population = basic_solve.solve(test_data, ns)
+        initial_population.sort(key=lambda x: x.get('cost'))
+        patches = initial_population[:nb]
+        best_cost = patches[0]['cost']
+        iterations = 0
+
+        # main loop
+        while True:
+            new_solutions = []
+            # search in elite and best solutions
+            for i in range(ne):
+                new_solution = local_search(patches[i], nre)
+                new_solutions.append(new_solution)
+            for i in range(ne, nb):
+                new_solution = local_search(patches[i], nrb)
+                new_solutions.append(new_solution)
+
+            # other bees are doing global search
+            global_searches = basic_solve.solve(test_data, ns - nb)
+            new_solutions.extend(global_searches)
+            new_solutions = new_solutions.sort(key=lambda x: x.get['cost'])
+            new_best_cost = new_solutions[0]
+
+            # we check stop conditions
+            if abs(new_best_cost - best_cost) < epsilon:
+                break
+            if iterations >= max_iters:
+                break
+            # new patches are our new solutions
+            patches = new_solutions[:nb]
+
+    def local_search(scout, foragers):
+        # original path contains all shops
+        original_path = []
+        solutions = []
+        for elem in scout['solution']:
+            original_path.append(elem[0])
+        for i in range(foragers):
+            solutions.append(generate_new_solution(original_path))
+        solution = min(solutions, key=lambda x: x.get('cost'))
+        return solution
+
+    def generate_new_solution(original_path):
+        # 0 - add shop, 1 - remove shop, 2 - permutation
+        while True:
+            new_path = original_path
+            distance = neighbourhood_size
+            while distance > 0:
+                operation = random.randint(0, 2)
+                if operation == 0:
+                    position = random.randint(1, len(shops))
+                    if position not in new_path:
+                        new_path.append(position)
+                        distance -= 1
+                elif operation == 1:
+                    elem_to_remove = random.sample(new_path, 1)
+                    if elem_to_remove in new_path:
+                        new_path.remove(elem_to_remove)
+                        distance -= 1
+                else:
+                    if distance >= 2:
+                        positions_list = [i for i in range(len(new_path))]
+                        positions = random.sample(positions_list, 2)
+                        new_path[positions[0]], new_path[positions[1]] = new_path[positions[1]], new_path[positions[0]]
+                        distance -= 2
+            solution = check_solution(new_path, shops, products_list)
+            if solution is not None:
+                print(solution)
+                cost = basic_solve.calculate_cost(solution, shops, start, weights)
+                return {'solution': solution, 'cost': cost}
+
+    solve()
+
+
+def check_solution(path, shops, products_list):
+    result = {}
+    for shop_number in path:
+        current_shop = shops[shop_number]
+        if len(products_list & current_shop['items']) != 0:
+            result[shop_number] = products_list & current_shop['items']
+            products_list = products_list - current_shop['items']
+    if len(products_list) == 0:
+        return result
+    return None
+
+
+with open('tests/normal2d.json', 'r') as file:
+    bees_algorithm(7, 2, 5, 3, 2, json.load(file), 3)
